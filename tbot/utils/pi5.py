13b6ae32221620fd8e3165020aa77bbbf50ea04d
CodeVampireYes <157810208+CodeVampireYes@users.noreply.github.com>
2025-03-28 03:02:37 +0100

    + metrics test

--- tbot/utils/pi5.py
+++ tbot/utils/pi5.py
@@ -1,13 +1,20 @@
 import subprocess
-
+import psutil
 
 def get_metrics_pi5():
     def cpu_temp():
         result = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True, text=True)
-        return result.stdout.strip().replace('temp=', '')
+        return result.stdout.strip().replace('temp=', '').replace("'C", "")
+
+    def cpu_usage():
+        return psutil.cpu_percent(interval=1)
+
+    def ram_usage():
+        mem = psutil.virtual_memory()
+        return mem.used / (1024 ** 2)
 
     return str(f"""
-    CPU: {cpu_temp()}
+    CPU: {cpu_temp()}°C | {cpu_usage()}%
     """)
 
 
