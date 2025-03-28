1b18218c52403cdf66359d5752fe39853bcf83ce
CodeVampireYes <157810208+CodeVampireYes@users.noreply.github.com>
2025-03-28 02:53:48 +0100

    + metrics test

--- tbot/utils/pi5.py
+++ tbot/utils/pi5.py
@@ -4,8 +4,7 @@ import subprocess
 def get_metrics_pi5():
     def cpu_temp():
         result = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True, text=True)
-        return result.stdout.strip().startswith('temp=', '')
-
+        return result.stdout.strip().replace('temp=', '')
 
     return str(f"""
     CPU: {cpu_temp()}
